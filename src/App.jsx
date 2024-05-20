3134079c2a77e75aa8545236f3eaff1895cc4c33
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-05-20 13:42:51 -0700

    changed file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Hero1 from "./Components/HeroSections/Hero1"
-import Feature1 from "./Components/FeatureSections/Feature1"
+import Hero1 from "./Layouts/HeroSections/Hero1"
+import Feature1 from "./Layouts/FeatureSections/Feature1"
 
 
 function App() {
     return (
         <div>
+            <Hero1 />
             <Feature1 />
         </div>
     )
